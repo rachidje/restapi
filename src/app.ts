abe14791f29514c36bdb79e8adc4c75cf2f62a0d
rachidWassati <rachid.jeffali@wassati.com>
2024-03-13 13:49:52 +0100

    deploiement sur heroku

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { apiRouter } from './routes/api';
+import cors from 'cors';
 require('./database')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use('/api', apiRouter);
 
