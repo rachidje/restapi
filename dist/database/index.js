e9f16d29da7a4f2c80f0dfa431a354a594a40802
rachidWassati <rachid.jeffali@wassati.com>
2024-03-13 12:14:54 +0100

    Deploiement sur heroku

--- dist/database/index.js
+++ dist/database/index.js
@@ -4,8 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const db_1 = require("../config/db");
-mongoose_1.default.connect(db_1.MONGO_URI).then(() => {
+mongoose_1.default.connect(process.env.MONGO_URI).then(() => {
     console.log("✅ Connected to database");
 }).catch((err) => {
     console.log(err);
